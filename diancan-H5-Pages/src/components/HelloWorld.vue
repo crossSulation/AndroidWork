97a409b3f0e72f402241273a9aa7e5f830777429
Luyao <luyao@LuyaodeMBP.lan>
2018-01-07 13:14:59 +0800

    add less elementUI

--- diancan-H5-Pages/src/components/HelloWorld.vue
+++ diancan-H5-Pages/src/components/HelloWorld.vue
@@ -17,6 +17,19 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <div class="my-container">
+      <h2 v-if="!canSee">{{ myword }}</h2>
+      <button @click="sayGood">SayGood</button>
+    </div>
+    <el-rate v-model="value3" show-text></el-rate>
+    <div class="block">
+  <span class="demonstration">有默认值</span>
+  <el-color-picker v-model="color1"></el-color-picker>
+</div>
+<div class="block">
+  <span class="demonstration">无默认值</span>
+  <el-color-picker v-model="color2"></el-color-picker>
+</div>
   </div>
 </template>
 
@@ -25,15 +38,26 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      myword: 'fuck you, jerk',
+      canSee: true,
+      value3: null,
+      color1: '#409EFF',
+      color2: null
+    }
+  },
+  methods: {
+    sayGood () {
+      alert('good day')
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
+<style lang="less" scoped>
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
@@ -46,5 +70,11 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.hello {
+  .my-container {
+    color: green;
+  }
 }
 </style>
